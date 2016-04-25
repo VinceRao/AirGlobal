4e749041c4d35527ffd35900bd7d64055f1d1cfc
vincerao524@gmail.com <vincerao524@gmail.com>
2016-04-24 17:06:07 -0700

    basic framework

---
+++ js/view/console.js
@@ -0,0 +1,16 @@
+'use stick';
+
+define([
+  'jquery',
+  'backbone',
+  'util'
+], function () {
+  var Console = function () {
+  };
+
+  Console.prototype = {
+    init : function () {
+    }
+  };
+  return Console;
+});
