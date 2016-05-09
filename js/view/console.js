08d92937cb8213f348c2542f98da3ba4a0717e92
xitaowang <xwang109@dons.usfca.edu>
2016-05-08 23:24:38 -0700

    Draw line

--- js/view/console.js
+++ js/view/console.js
@@ -8,6 +8,7 @@ define([
   'view/uschina',
   'model/berkeley',
   'model/embassy',
+    'view/charts/linechart',
   'd3-legend',
 ], function (
   $,
@@ -16,7 +17,8 @@ define([
   d3,
   USChina,
   Berkeley,
-  Embassy
+  Embassy,
+  LineChart
 ) {
   var Console = function () {
   };
@@ -40,6 +42,39 @@ define([
           var b = options.data.berkeley;
           var e = options.data.embassy;
           //console.log(options.data.embassy);
+         // an array of all cities
+          var cities = options.data.berkeley.getAllCities();
+          console.log(cities);
+
+         //get all days data for a city
+          var beijingdata = options.data.berkeley.getAllDayForCity("Shanghai");
+          console.log(beijingdata);
+
+          // get all days data for a set of cities
+          var somecities = ["Anshan","Beijing","Zunyi"] //0, 6, 181 in cities
+          var somecitiesAlltime = options.data.berkeley.getCitiesData(somecities);
+          console.log(somecitiesAlltime);
+
+          //get all cities data for a day
+          var d0 = options.data.berkeley.start; //in microseconds, day 0
+          var d1 = 1; //day 1
+          var allcityDay0 = options.data.berkeley.getAllCityAtDay(d0);
+          var allcityDay1 = options.data.berkeley.getAllCityAtDay(d1);
+          console.log(allcityDay0);
+          console.log(allcityDay1);
+
+         //get all cities data for a time range, consecutive
+          var start = options.data.berkeley.start; //in microseconds
+          var end = start + options.data.berkeley.interval * 3;
+          var allcityTimeRange = options.data.berkeley.getTimeRangeData(start, end);
+          console.log(allcityTimeRange);
+
+          //get certain timepoints for a set of cities, return a map with timepoints, indices, and city-array pairs
+          //timepoints can be indices or microseconds
+          var timepoints = [options.data.berkeley.start, 1, 121];
+          //var somecities = ["Anshan","Beijing","Zunyi"] //0, 6, 181 in cities //already defined above
+          var selecteddata = options.data.berkeley.getCitiesTimepoints(somecities, timepoints);
+          console.log(selecteddata);
 
           var shanghaidata = b.getAllDayForCity("Shanghai");
           console.log(shanghaidata);
@@ -117,6 +152,8 @@ define([
  */
 /*        uschinaContainer = new USChina({root : options.rootView, data : options.data})
         uschinaContainer.render();*/
+          var line = new LineChart(options);
+          line.render();
       });
     }
   };
